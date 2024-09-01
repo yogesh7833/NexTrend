157d4acc47152774c082345919f8f33aaac86144
yogesh7833 <yogeshkus783@gmail.com>
2024-09-01 20:16:48 +0530

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Placeorder from './pages/Placeorder'
+import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer/>
+      <Navbar/>
+      <SearchBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/Cart' element={<Cart/>}/>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/place-order' element={<Placeorder/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
